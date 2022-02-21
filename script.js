c3099abfc636e8df4f7c08b3016da75b9dba0cd5
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2022-02-22 04:22:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
  
 var settingsmenu = document.querySelector(".settings-menu");
 var darkbtn = document.getElementById("dark-btn");
+
+
+function settingsmenutoggle(){
+ 
+  settingsmenu.classList.toggle("settings-menu-height")
+}
